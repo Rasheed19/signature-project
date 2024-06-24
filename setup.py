3af9ee838525b7e789ab0812c907beec2cf48bae
Rasheed19 <ibraheem.abdulrasheed@gmail.com>
2024-06-24 18:42:23 +0100

    update setup file

--- setup.py
+++ setup.py
@@ -23,18 +23,4 @@ setup(
     ],
     packages=find_packages(),
     python_requires=">=3.7",
-    install_requires=[
-        "iisignature==0.24",
-        "pandas==2.0.2",
-        "numpy==1.25.2",
-        "matplotlib==3.7.1",
-        "scipy==1.10.1",
-        "scikit-learn==1.2.2",
-        "rrct==1.0.4",
-        "DateTime==5.1",
-        "h5py==3.8.0",
-        "xgboost==1.7.5",
-        "seaborn==0.12.2",
-        "pingouin==0.5.3",
-    ],
 )
